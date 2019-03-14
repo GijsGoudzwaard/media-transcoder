1279b8c90bfb52daa58d4cbc555d25f8dd7816bb
Gijs G <gijs.goudzwaard@gmail.com>
2019-03-14 19:25:51 +0100

    Converted to spaces from tabs

--- index.py
+++ index.py
@@ -7,30 +7,30 @@ ffprobe = "ffprobe -v quiet -select_streams v:0 -show_entries stream=codec_name 
 mime = magic.Magic(mime=True)
 
 def isVideo(file):
-	file_mime = mime.from_file(file)
+    file_mime = mime.from_file(file)
 
-	return "video" in file_mime
+    return "video" in file_mime
 
 def getNonH264Files():
-	non_h264 = []
+    non_h264 = []
 
-	for dirpath,_,filenames in os.walk("./../../Thor/Media"):
-		for f in filenames:
-			path = os.path.abspath(os.path.join(dirpath, f))
+    for dirpath,_,filenames in os.walk("./../../Thor/Media"):
+        for f in filenames:
+            path = os.path.abspath(os.path.join(dirpath, f))
 
-			if isVideo(path):
-				process = subprocess.Popen('%s "%s"' % (ffprobe, path), shell=True, stdout=subprocess.PIPE)
-				output, error = process.communicate()
+            if isVideo(path):
+                process = subprocess.Popen('%s "%s"' % (ffprobe, path), shell=True, stdout=subprocess.PIPE)
+                output, error = process.communicate()
 
-				if not 'h264' in output.decode("utf-8"):
-					non_h264.append(path)
+                if not 'h264' in output.decode("utf-8"):
+                    non_h264.append(path)
 
-	return non_h264
+    return non_h264
 
 non_h264 = getNonH264Files()
 
 for path in non_h264:
-	print(path)
+    print(path)
 
 
 print('Non h264 files: %d' % len(non_h264))
