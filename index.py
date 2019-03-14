c750c5e2cc7071371f43c1a19a13e4279c1cddbb
Gijs G <gijs.goudzwaard@gmail.com>
2019-03-14 18:36:46 +0100

    Fixed errors that would occur with special characters

--- index.py
+++ index.py
@@ -1,24 +1,37 @@
 import os
+import magic
 import subprocess
 
-ffprobe = "ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "
+ffprobe = "ffprobe -v quiet -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "
 
-def getNonH264Paths():
+mime = magic.Magic(mime=True)
+
+def isVideo(file):
+	file_mime = mime.from_file(file)
+
+	return "video" in file_mime
+
+def getNonH264Files():
 	non_h264 = []
 
-	for dirpath,_,filenames in os.walk("./../Media"):
+	for dirpath,_,filenames in os.walk("./../../Thor/Media"):
 		for f in filenames:
-			path = os.path.abspath(os.path.join(dirpath, f)).replace(' ', '\ ')
+			path = os.path.abspath(os.path.join(dirpath, f))
 
-			process = subprocess.Popen(ffprobe + path, shell=True, stdout=subprocess.PIPE)
-			output, error = process.communicate()
+			if isVideo(path):
+				process = subprocess.Popen('%s "%s"' % (ffprobe, path), shell=True, stdout=subprocess.PIPE)
+				output, error = process.communicate()
 
-			if not 'h264' in output.decode("utf-8"):
-				non_h264.append(path)
+				if not 'h264' in output.decode("utf-8"):
+					non_h264.append(path)
 
 	return non_h264
 
-# getNonH264Paths()
+non_h264 = getNonH264Files()
 
-for path in getNonH264Paths():
+for path in non_h264:
 	print(path)
+
+
+print('Non h264 files: %d' % len(non_h264))
+
