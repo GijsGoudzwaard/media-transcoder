86e893c730d67b5dff267e5e556ee6466279c76b
Gijs G <gijs.goudzwaard@gmail.com>
2019-03-28 22:15:41 +0100

    All video files are now properly transcoded to h264. Updated README. Added new arguments.

--- README.md
+++ README.md
@@ -1,25 +1,35 @@
+# Media transcoder
+
+Transcode all video files within a directory to 'direct play'. This script will search for all video files within a directory and check its codec. If the codec is not h264, then it will be transcoded to h264.
+
+This is especially usefull for a raspberry pi media server where all media from 1080p and up needs to be transcoded for direct play. This script does not copy any subtitles.
+
 ## Setup
 
 Both python 2 and python 3 are supported.
 
-```
+```bash
 pip install -r requirements.txt
 ```
 
-## Transcoding
-
-```
-python index.py --path=./../Media
-```
-
-## Options
+## Usage
 
 All available options are specified below. Of all these options, only `path` is **required**.
 
-```
-python index.py
+```text
+usage: python index.py --path=PATH [--output=OUTPUT] [--override=False] [--threads=1]
+
+Arguments
     --path (required)
         Location which will be searched for video's and transcoded if they are found.
+
+    --output
+        The name of the transcoded file. The file will be placed in the same
+        directory as the original file. Also the file will be forced to .mp4.
+        Default is the name of the original file.
+
+    --overwrite
+        Overwrite the original file with the newly transcoded file. Default is False.
 
     --threads
         How many CPU cores you'd like to use for transcoding. Default is 1.
--- index.py
+++ index.py
@@ -4,7 +4,18 @@ import magic
 import subprocess
 from tqdm import tqdm
 
-def getArg(search, fallback = None):
+
+def getArg(search, fallback=None):
+    """Retrieve an argument. If the argument doesn't exist, return the
+    fallback. If the fallback doesn't exist and the argument doesn't
+    exist, exit the program since a required argument isn't passed.
+
+    Args:
+        search: a string that is being used to search if an argument exists
+        fallback: a value that will be returned if the argument doesn't exist
+    Returns:
+        The value of the argument or the given fallback.
+    """
     args = sys.argv[1:]
     found_arg = False
 
@@ -24,61 +35,102 @@ def getArg(search, fallback = None):
 
     return found_arg
 
+
+# Command that will return the codec of a video file.
 ffprobe = "ffprobe -v quiet -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "
 
+# The directory where video files will be searched and transcoded.
 DIR = getArg('path')
 
+# Since we need a directory to search / trancode in,
+# exit the program if it doesn't exist.
 if (DIR == None):
     sys.exit('Path is required.')
 
 mime = magic.Magic(mime=True)
 
-def isVideo(file):
-  file_mime = mime.from_file(file)
 
-  return "video" in file_mime
+def isVideo(file):
+    """Check if the given file is a video or not.
+
+    Args:
+        file: a string of an absolute path where a file is stored
+    Returns:
+        A boolean
+    """
+    file_mime = mime.from_file(file)
+
+    return "video" in file_mime
+
 
 def getFileCount():
-  count = 0
-  for dirpath, _, filenames in os.walk(DIR):
-    for f in filenames:
-      count += 1
+    """Count how many files there are within a directory.
 
-  return count
+    Returns:
+        The amount of documents there are in a directory
+    """
+    count = 0
+
+    for dirpath, _, filenames in os.walk(DIR):
+        for f in filenames:
+            count += 1
+
+    return count
+
 
 def getNonH264Files():
-  file_count = getFileCount()
+    """Get all video files whose codec is not h264.
 
-  non_h264 = []
+    Returns:
+        List of video files whose codec is not h264.
+    """
+    file_count = getFileCount()
 
-  with tqdm(total=file_count, unit=' Files', unit_scale=True, unit_divisor=1024) as pbar:
-    for dirpath, _, filenames in os.walk(DIR):
-      for f in filenames:
-        path = os.path.abspath(os.path.join(dirpath, f))
+    non_h264 = []
 
-        pbar.update(1)
+    with tqdm(total=file_count, unit=' Files', unit_scale=True, unit_divisor=1024) as pbar:
+        for dirpath, _, filenames in os.walk(DIR):
+            for f in filenames:
+                path = os.path.abspath(os.path.join(dirpath, f))
 
-        if isVideo(path):
-          process = subprocess.Popen('%s "%s"' % (ffprobe, path), shell=True, stdout=subprocess.PIPE)
-          output, error = process.communicate()
+                pbar.update(1)
 
-          if not 'h264' in output.decode("utf-8"):
-            non_h264.append(path)
+                if isVideo(path):
+                    process = subprocess.Popen('%s "%s"' % (ffprobe, path), shell=True, stdout=subprocess.PIPE)
+                    output, error = process.communicate()
 
-  return non_h264
+                if not 'h264' in output.decode("utf-8"):
+                    non_h264.append(path)
+
+    return non_h264
 
 print("Fetching to be transcoded files, this could take a while depending on how large your library is...")
 non_h264 = getNonH264Files()
 
 print("Non h264 files: %d" % len(non_h264))
 
-# print(getFileCount())
+# Command that will transcode the video file to a 'direct play' format.
+transcode = 'ffmpeg -i "%s" -loglevel quiet -map 0:a -map 0:v -c:v libx264 -preset ultrafast -crf 23 -tune film -b:v 8M -maxrate:v 8M -bufsize:v 8M -c:a aac -ac 2 -ab 256K -threads %d "%s" -y'
 
-# transcode = 'ffmpeg -i \'%s\' -map 0:a -map 0:s -map 0:v -c:v libx264 -preset ultrafast -crf 23 -tune film -b:v 8M -maxrate:v 8M -bufsize:v 8M -c:a aac -ac 2 -ab 256K -c:s mov_text -threads 4 %s -y'
+print('Transcoding...')
 
-# print('Transcoding...')
+total_nonh264 = len(non_h264)
+i = 0
 
-# for path in non_h264:
-#     print('[Transcoding] - %s' % path)
+for path in non_h264:
+    print('[Transcoding %d/%d] - %s' % (i, total_nonh264, path))
 
-# transcoding = subprocess.Popen(transcode % ('input.mkv', 'output.mp4'), shell=True, stdout=subprocess.PIPE)
+    i += 1
+
+    splitted_path = path.split('/')
+    input_dir = '/'.join(splitted_path[:-1])
+
+    file_name = '.'.join(os.path.basename(path).split('.')[:-1])
+    file_name = getArg('output', file_name)
+
+    command = transcode % (path, int(getArg('threads', 1)), ('%s/%s.mp4' % (input_dir, file_name)))
+    transcoding = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    output, error = transcoding.communicate()
+
+    if getArg('overwrite', False):
+        os.remove(path.replace("'", "\'"))
